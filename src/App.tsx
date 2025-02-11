436e60e15ded6528dc77d5d51136e127eea304a8
maxcore25 <mkorobov2002@mail.ru>
2025-02-12 00:50:30 +0300

    Add heart icon and fix image size in carousel

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { Tabs, TabsList, TabsTrigger } from '@/components/ui/tabs';
 import { Button } from './components/ui/button';
-import { ChevronRight } from 'lucide-react';
+import { ChevronRight, Heart } from 'lucide-react';
 
 function App() {
   return (
@@ -126,14 +126,19 @@ function App() {
             </Button>
           </div>
           <div className='flex max-w-[100%] items-center gap-[20px] overflow-x-auto'>
-            <a href='#' className='block'>
-              <img src='/1.webp' alt='shoes' className='max-w-[150px]' />
+            <a href='#' className='relative block'>
+              <Heart className='absolute left-0 top-0 z-10' />
+              <img
+                src='/1.webp'
+                alt='shoes'
+                className='max-h-[96px] max-w-[150px]'
+              />
               <div className='grid gap-[4px] px-[10px]'>
                 <div className='text-[20px] font-bold leading-[20px] tracking-[-0.03rem]'>
                   14 443
                   <span className='text-[12px]'>₽</span>
                 </div>
-                <div>
+                <div className='text-[11px] leading-[16px]'>
                   <span className='mr-[2px] rounded-[4px] bg-[#3e3e3e] px-[3px] text-[11px] leading-[16px] text-white'>
                     7 222
                     <span>₽</span> × 2
@@ -149,22 +154,27 @@ function App() {
                 </div>
               </div>
             </a>
-            <a href='#' className='block'>
-              <img src='/2.webp' alt='shoes' className='max-w-[150px]' />
+            <a href='#' className='relative block'>
+              <Heart className='absolute left-0 top-0 z-10' />
+              <img
+                src='/2.webp'
+                alt='shoes'
+                className='max-h-[96px] max-w-[150px]'
+              />
               <div className='grid gap-[4px] px-[10px]'>
                 <div className='text-[20px] font-bold leading-[20px] tracking-[-0.03rem]'>
-                  14 443
+                  24 983
                   <span className='text-[12px]'>₽</span>
                 </div>
-                <div>
-                  <span className='mr-[2px] rounded-[4px] bg-[#3e3e3e] px-[3px] text-[11px] leading-[16px] text-white'>
-                    7 222
+                <div className='text-[11px] leading-[16px]'>
+                  <span className='mr-[2px] rounded-[4px] bg-[#3e3e3e] px-[3px] text-white'>
+                    12 492
                     <span>₽</span> × 2
                   </span>
                   в сплит
                 </div>
                 <div className='text-[14px] font-medium leading-[17px]'>
-                  Nike React Hyperdunk 2017 Low
+                  THE NORTH FACE GLENCLYFFE{' '}
                 </div>
                 <div className='flex gap-2 text-[11px]'>
                   21 день
