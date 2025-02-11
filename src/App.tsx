0afcf6e3176957e504b7f4796a6abd237837dbc3
maxcore25 <mkorobov2002@mail.ru>
2025-02-12 00:36:42 +0300

    Delete comments

--- src/App.tsx
+++ src/App.tsx
@@ -10,10 +10,6 @@ function App() {
           <TabsTrigger value='account'>Мужчинам</TabsTrigger>
           <TabsTrigger value='password'>Женщинам</TabsTrigger>
         </TabsList>
-        {/* <TabsContent value='account'>
-          Make changes to your account here.
-        </TabsContent>
-        <TabsContent value='password'>Change your password here.</TabsContent> */}
       </Tabs>
 
       <main className='m-[0_auto] grid max-w-[1432px] gap-[10px]'>
