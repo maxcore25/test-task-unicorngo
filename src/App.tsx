c009b7e6052c066ce9ae7209eda2fc3e744566f9
maxcore25 <mkorobov2002@mail.ru>
2025-02-12 00:35:57 +0300

    Add scroll to carousel

--- src/App.tsx
+++ src/App.tsx
@@ -129,7 +129,7 @@ function App() {
               <ChevronRight />
             </Button>
           </div>
-          <div className='flex items-center gap-[20px]'>
+          <div className='flex max-w-[100%] items-center gap-[20px] overflow-x-scroll'>
             <a href='#' className='block'>
               <img src='/1.webp' alt='shoes' className='max-w-[150px]' />
               <div className='grid gap-[4px] px-[10px]'>
